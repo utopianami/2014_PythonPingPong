8c89ba70e5075129a0e38a487edf4f3e458b1190
TaekSoon <taeksoon.jang@gmail.com>
2014-03-03 00:54:17 +0900

    slight modification

--- app/static/js/custom.js
+++ app/static/js/custom.js
@@ -3,7 +3,11 @@ window.addEventListener("load", function(e) {
     var idObj = {
         signUpValidation: "signup-form",
         playerRegister: "player-register-btn"
-    }
+    };
+
+    var classObj = {
+        sendMatchInvitation: "send-match-invitation"
+    };
 
     document.addEventListener("click", function(e) {
         var target = e.target;
@@ -23,10 +27,17 @@ window.addEventListener("load", function(e) {
                 e.preventDefault();
                 if(checkPwdValidation() === true) {
                     target.submit();
-                };
+                }
+                break;
+        }
+
+        switch (target.class) {
+            case classObj.sendMatchInvitation:
+                alert("아직 준비중입니다. 정식 릴리즈때 찾아뵙겠습니다.");
                 break;
         }
     }, false);
+
 }, false);
 
 function checkPwdValidation() {
