8532b91924c23c71943f22248a8827c95cfcc774
TaekSoon <taeksoon.jang@gmail.com>
2014-03-02 23:00:15 +0900

    separate html and javascript personal page test

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os.path
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/newDB13.db'
+SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/newDB134.db'
 
 ADMINS = frozenset(['youremail@yourdomain.com'])
 SECRET_KEY = 'SecretKeyForSessionSigning'
